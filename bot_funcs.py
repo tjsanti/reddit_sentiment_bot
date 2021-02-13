c918dc6d913db1989c92aff5d870c6587dfe3672
trevor-santiago <tjsantiago@dons.usfca.edu>
2021-02-12 17:00:08 -0800

    initial commit

---
+++ bot_funcs.py
@@ -0,0 +1,65 @@
+import time
+
+import numpy as np
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+
+def fetch_comments(author):
+    '''Gathers comments from Redditor instance'''
+
+    return list(author.comments.new(limit=1_000))
+
+
+def process_comments(comments, num_comments):
+    '''Computes average sentiment score for an array of reddit Comments'''
+
+    analyzer = SentimentIntensityAnalyzer()
+    sum_scores = 0
+    print(f'Processing {num_comments} comments')
+    begin = time.time()
+
+    for comment in comments:
+        text = comment.body
+        score = analyzer.polarity_scores(text)['compound']
+        sum_scores += score
+
+    end = time.time()
+    avg_score = sum_scores / num_comments
+    print(
+        f'Finished in {end-begin:.3f} seconds. Average score {avg_score:.2f}')
+
+    return avg_score
+
+
+def reply(mention, parent_author, avg_score, num_comments):
+    '''Replies to a reddit mention and then deletes it from the inbox'''
+
+    positive_replies = [
+        f"/u/{parent_author} might be having a bad day."
+    ]
+    negative_replies = [
+        f"I think /u/{parent_author} might be a south pole elf."
+    ]
+
+    if avg_score > 0:
+        opener = np.random.choice(positive_replies)
+    else:
+        opener = np.random.choice(negative_replies)
+
+    score_report = f'\n\nTheir last {num_comments} comments had an average sentiment of {avg_score:.3f}.'
+
+    if avg_score >= 0.15:
+        score_report += ' (Pretty Positive)' + '\n'*4
+    elif avg_score >= -0.15:
+        score_report += ' (Mostly Neutral)' + '\n'*4
+    else:
+        score_report += ' (Pretty Negative)' + '\n'*4
+
+    signature = '___' + '\n\n' + \
+        '^(I am a bot. If you believe there is an issue, please pm me so I can pass it on to my creator)'
+    reply_comment = opener + score_report + signature
+    mention.reply(reply_comment)
+    mention.delete()
+    print('Mention replied to and deleted')
+
+    return
---
+++ sentiment_bot.py
@@ -0,0 +1,56 @@
+import json
+import time
+import sys
+
+import praw
+
+from bot_funcs import fetch_comments, process_comments, reply
+
+# pass json file with api keys and bot account info
+cert_file = sys.argv[1]
+with open(cert_file, 'r') as f:
+    data = json.load(f)
+
+client_id = data['client_id']
+secret = data['secret']
+username = data['username']
+password = data['password']
+user_agent = data['user_agent']
+
+reddit = praw.Reddit(
+    client_id=client_id,
+    client_secret=secret,
+    user_agent=user_agent,
+    username=username,
+    password=password
+)
+
+
+def process(mention):
+    '''
+    Gathers comments from parent author,
+    and passes them on for processing and reply
+    '''
+    author = mention.author
+    parent_author = mention.parent().author
+    print(f'/u/{author} wants to see if /u/{parent_author} is having a bad day...')
+    comments = fetch_comments(parent_author)
+    num_comments = len(comments)
+    avg_score = process_comments(comments, num_comments)
+    reply(mention, parent_author, avg_score, num_comments)
+
+# get 10 most recent messages
+messages = reddit.inbox.unread(limit=10)
+
+flag = False
+for message in messages:
+    flag = True
+    # only process mentions
+    if username in message.body:
+        process(message)
+    else:
+        message.mark_read()
+        message.delete()
+
+if not flag:
+    print('No new comments.')
